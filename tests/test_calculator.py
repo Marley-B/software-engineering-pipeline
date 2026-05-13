6c4de67d12dfdaa1d0da18a711f5d3ff0a5fa08a
Marley-B <marley.bano01@estudiant.upf.edu>
2026-05-13 15:13:02 +0200

    test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,8 @@
-from calculator import add, sub
+from calculator import add, sub, square
 
 def test_add():
     assert add(2, 3) == 5
 def test_sub():
     assert sub(10, 4) == 6
+def test_square():
+    assert square(2, 3) == 8
