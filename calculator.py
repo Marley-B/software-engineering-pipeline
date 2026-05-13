f67ea0bd45f295e519dc55ba02103d94b755f41e
Aniol Marcet <aniol.marcet@gmail.com>
2026-05-13 15:26:44 +0200

    Add root operation

--- calculator.py
+++ calculator.py
@@ -1,4 +1,10 @@
+
 def add(a, b):
     return a + b
 def sub(a, b):
     return a - b
+
+def root(a, b):
+    if(b == 0):
+        return None
+    return a **(1/b)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,11 @@
-from calculator import add, sub
+from calculator import add, sub, root
 
 def test_add():
     assert add(2, 3) == 5
 def test_sub():
     assert sub(10, 4) == 6
+
+def test_root():
+    assert root(4, 2) == 2
+    assert root(3, 0) == None
+    assert root(27, 3) == 0
